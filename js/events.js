1aaf567788ff085c08714f1531f0adc5017fc386
Man Hong Lee <manhong323@gmail.com>
2017-10-24 03:28:35 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
-//define functions here
+// //define functions here
+var getIt = function(){
+  alert('hey');
+}
+
+var frameIt = function(){
+  $('img').addClass('tasty');
+}
+
+var submitIt = function(){
+  alert('Your form is going to be submitted now.');
+}
+
+var pressIt = function(key){
+  if(key.which === 71) {
+    alert('g was pressed');
+  }
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').click(getIt);
+  $('img').on('load', frameIt);
+  $(':submit').submit(submitIt);
+  $('#typing').on('keydown', pressIt);
 });
